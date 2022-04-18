617b0eb3fe92016579fcdddc9a5d5f30fe1c1516
marouaJerbi <36313623+marouaJerbi@users.noreply.github.com>
2022-04-18 15:57:34 -0400

    add filter for region for viz 1
    add filter for region for viz 1

--- code/src/app.py
+++ code/src/app.py
@@ -8,40 +8,42 @@ import preprocess as preproc
 import sankey
 import stackedBarChart
 import heatmap
-#import pymsgbox
+# import pymsgbox
 import barchart
 
 app = dash.Dash(__name__)
 app.title = 'Projet | INF8808'
 
-df_file = "donnees_culturelles_synapseC_2.csv"
+df_file = "assets/donnees_culturelles_synapseC_2.csv"
 df = preproc.to_df(df_file)
 # data preparation
-repartition_region = preproc.to_df("repartion_region.csv")
+repartition_region = preproc.to_df("assets/repartion_region.csv")
 clusters = preproc.add_cluster(repartition_region)
 
 new_df = preproc.add_clusters(df, clusters)
 
-df_2016 = preproc.group_by_year_month(df, 2021, 7)
+df_2016 = preproc.group_by_year_month(df, 2016, 12)
 
-df_file_preprocessed = "df.csv"
+df_file_preprocessed = "assets/df.csv"
 df_preprocessed = preproc.to_df(df_file_preprocessed)
 
-clus_est_gratuit_data=preproc.group_by_column2_count(df, 'groupe','est_gratuit')
-df_barchart=preproc.data_prepartion_barchart_gratuit(new_df,clus_est_gratuit_data)
+clus_est_gratuit_data = preproc.group_by_column2_count(
+    df, 'groupe', 'est_gratuit')
+df_barchart = preproc.data_prepartion_barchart_gratuit(
+    new_df, clus_est_gratuit_data)
 
 
 fig1 = stackedBarChart.stackedBarChart(df_2016)
 fig2 = sankey.sankey_diagram_g_cat(new_df)
 fig3 = sankey.sankey_diagram_r_cat(new_df, 'Centre')
-#fig4 = sankey.sankey_diagram_g_scat(new_df, 'Musique')
-fig4 = heatmap.make_heatmap(df_preprocessed, years=set([2019,2020]))
+# fig4 = sankey.sankey_diagram_g_scat(new_df, 'Musique')
+fig4 = heatmap.make_heatmap(df_preprocessed, years=set([2019, 2020]))
 fig5 = sankey.sankey_diagram_r_cat(new_df, 'Sud')
 fig6 = sankey.sankey_diagram_g_scat(new_df, 'ArtsVisuels')
-fig7=barchart.barchart_gratuit(df_barchart)
+fig7 = barchart.barchart_gratuit(df_barchart)
 
 
-#fig4.write_html("index4.html")
+# fig4.write_html("index4.html")
 def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
 
     return html.Div(className='content', children=[
@@ -76,11 +78,34 @@ def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
                     html.Div([
                         dcc.Dropdown(
                             options=[
-                                {'label': 'Montréal', 'value': 'Montréal'},
+                                {'label': 'Abitibi-Témiscamingue',
+                                    'value': 'Abitibi-Témiscamingue'},
+                                {'label': 'Bas-Saint-Laurent',
+                                    'value': 'Bas-Saint-Laurent'},
+                                {'label': 'Capitale-Nationale',
+                                    'value': 'Capitale-Nationale'},
+                                {'label': 'Centre-du-Québec',
+                                    'value': 'Centre-du-Québec'},
+                                {'label': 'Chaudière-Appalaches',
+                                    'value': 'Chaudière-Appalaches'},
+                                {'label': 'Côte-Nord', 'value': 'Côte-Nord'},
+                                {'label': 'Gaspésie et îles-de-la-Madeleine',
+                                    'value': 'Gaspésie et îles-de-la-Madeleine'},
+                                {'label': 'Lanaudière', 'value': 'Lanaudière'},
+                                {'label': 'Laurentides', 'value': 'Laurentides'},
                                 {'label': 'Laval', 'value': 'Laval'},
-                                {'label': 'Estrie', 'value': 'Estrie'}
+                                {'label': 'Mauricie', 'value': 'Mauricie'},
+                                {'label': 'Montérégie', 'value': 'Montérégie'},
+                                {'label': 'Montréal', 'value': 'Montréal'},
+                                {'label': 'Nord-du-Québec',
+                                    'value': 'Nord-du-Québec'},
+                                {'label': 'Outaouais', 'value': 'Outaouais'},
+                                {'label': 'Saguenay-Lac-Saint-Jean',
+                                    'value': 'Saguenay-Lac-Saint-Jean'},
+                                {'label': 'Toutes les régions',
+                                    'value': 'Toutes les régions'}
                             ],
-                            value='Montréal',
+                            value='Toutes les régions',
                             id='dropdownRegion'
                         ),
                     ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
@@ -96,6 +121,8 @@ def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
                           ),
                           className='graph',
                           id='viz_1'),
+                html.Label(['Choisir le mois'], style={
+                    'font-weight': 'bold'}),
                 dcc.Slider(
                     id='MonthsSlider',
                     min=0,
@@ -103,7 +130,22 @@ def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
                     step=1,
                     value='12'
                 ),
-                html.Label(['Month'], style={'font-weight': 'bold'})
+                html.Label(['Choisir le prix en CAD'],
+                           style={'font-weight': 'bold'}),
+                html.Div([
+                    dcc.Input(type='text', id='minPrice'),
+                    dcc.RangeSlider(
+                        id='PriceSlider',
+                        min=0,
+                        max=1000,
+                        value=[0, 1000],
+                        step=250,
+                        allowCross=False
+                    ),
+                    dcc.Input(type='text', id='maxPrice'),
+                ],
+                    style={"display": "grid", "grid-template-columns": "10% 40% 10%"})
+
             ]),
             # html.Div(className='viz-container', children=[
             #     dcc.Graph(
@@ -147,41 +189,47 @@ def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
                 )
             ]),
             html.Div(className='img-quebec', children=[
-                    html.Img(src="assets/Quebec_clusters.PNG", 
-                    id="quebec-cluster"),
-                    html.Div(className="overlay",children=[
-                        html.Div('Les centres culturels sont les suivants:',className="title"),
-                        html.Div(children=['Nord du Québec :',
-                            html.Div(className="text-nord", children=[
-                                'Abitibi-Temiscamingue',
-                                html.Div('Capitale-Nationale'),
-                                html.Div('Côte-Nord'),
-                                html.Div('Mauricie'),
-                                html.Div('Nord-du-Québec et de la Baie-James'),
-                                html.Div('Saguenay-Lac-Saint-Jean')
-                            ], style={'color':'black'}),
-                        ],className="title-nord"),
-                        html.Div(children=['Centre du Québec :',
-                            html.Div(className="text-centre", children=[
-                                'Centre-du-Québec',
-                                html.Div('Lanaudière'),
-                                html.Div('Laurentides'),
-                                html.Div('Laval'),
-                                html.Div('Outaouais')
-                            ], style={'color':'black'}),
-                        ],className="title-centre"),
-                        html.Div(children=['Sud du Québec :',
-                            html.Div(className="text-sud", children=[
-                                'Bas-Saint-Laurent',
-                                html.Div('Chaudière-Appalaches'),
-                                html.Div('Estrie'),
-                                html.Div('Gaspésie et îles-de-la-Madeleine'),
-                                html.Div('Montérégie')
-                            ], style={'color':'black'}),
-                        ],className="title-sud"),
-                        html.Div(children=['Montréal'],className="title-montreal")
-                    ])
-                ]),
+                html.Img(src="assets/Quebec_clusters.PNG",
+                             id="quebec-cluster"),
+                html.Div(className="overlay", children=[
+                    html.Div(
+                        'Les centres culturels sont les suivants:', className="title"),
+                    html.Div(children=['Nord du Québec :',
+                                       html.Div(className="text-nord", children=[
+                                           'Abitibi-Témiscamingue',
+                                           html.Div('Capitale-Nationale'),
+                                           html.Div('Côte-Nord'),
+                                           html.Div('Mauricie'),
+                                           html.Div(
+                                               'Nord-du-Québec et de la Baie-James'),
+                                           html.Div(
+                                               'Saguenay-Lac-Saint-Jean')
+                                       ], style={'color': 'black'}),
+                                       ], className="title-nord"),
+                    html.Div(children=['Centre du Québec :',
+                                       html.Div(className="text-centre", children=[
+                                           'Centre-du-Québec',
+                                           html.Div('Lanaudière'),
+                                           html.Div('Laurentides'),
+                                           html.Div('Laval'),
+                                           html.Div('Outaouais')
+                                       ], style={'color': 'black'}),
+                                       ], className="title-centre"),
+                    html.Div(children=['Sud du Québec :',
+                                       html.Div(className="text-sud", children=[
+                                           'Bas-Saint-Laurent',
+                                           html.Div(
+                                               'Chaudière-Appalaches'),
+                                           html.Div('Estrie'),
+                                           html.Div(
+                                               'Gaspésie et îles-de-la-Madeleine'),
+                                           html.Div('Montérégie')
+                                       ], style={'color': 'black'}),
+                                       ], className="title-sud"),
+                    html.Div(children=['Montréal'],
+                             className="title-montreal")
+                ])
+            ]),
             html.Div(className='viz-container', children=[
                 dcc.Graph(
                     figure=fig2,
@@ -228,7 +276,6 @@ def init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6):
 app.layout = init_app_layout(fig1, fig2, fig3, fig4, fig5, fig6)
 
 
-
 with open('indexViz_alpha.html', 'a') as f:
     f.write(fig1.to_html(full_html=False, include_plotlyjs='cdn'))
     f.write(fig2.to_html(full_html=False, include_plotlyjs='cdn'))
@@ -238,71 +285,29 @@ with open('indexViz_alpha.html', 'a') as f:
     f.write(fig6.to_html(full_html=False, include_plotlyjs='cdn'))
     f.write(fig7.to_html(full_html=False, include_plotlyjs='cdn'))
 
-# fig1.update_layout(
-#     updatemenus=[
-#         dict(
-#             buttons=list([
-#                 dict(
-#                     args=["2021", "5"],
-#                     label="2016",
-#                     method="restyle"
-#                 ),
-#                 dict(
-#                     args=["year", "2017"],
-#                     label="2017",
-#                     method="restyle"
-#                 )
-#             ]),
-#             direction="down",
-#             pad={"r": 10, "t": 10},
-#             showactive=True,
-#             x=0.1,
-#             xanchor="left",
-#             y=1.1,
-#             yanchor="top"
-#         ),
-#         dict(
-#             buttons=list([
-#                 dict(
-#                     args=["month", "1"],
-#                     label="Janvier",
-#                     method="restyle"
-#                 ),
-#                 dict(
-#                     args=["month", "2"],
-#                     label="Fevrier",
-#                     method="restyle"
-#                 )
-#             ]),
-#             direction="down",
-#             pad={"r": 10, "t": 10},
-#             showactive=True,
-#             x=0.1,
-#             xanchor="right",
-#             y=1.1,
-#             yanchor="top"
-#         )
-#     ]
-# )
-    
-#fig1.write_html("indexViz1.html")
-#fig2.write_html("indexFig2.html")
-##fig3.write_html("indexFig3.html")
-#fig4.write_html("indexFig4.html")
-#fig5.write_html("indexFig5.html")
-#fig6.write_html("indexFig6.html")
 
-@app.callback(
+@ app.callback(
     Output('viz_1', 'figure'),
     [Input(component_id='dropdownYear', component_property='value')],
-    [Input(component_id='MonthsSlider', component_property='value')]
+    [Input(component_id='MonthsSlider', component_property='value')],
+    [Input(component_id='dropdownRegion', component_property='value')],
+    #[Input(component_id='minPrice', component_property='value')],
+    #[Input(component_id='PriceSlider', component_property='value')],
+    #[Input(component_id='maxPrice', component_property='value')],
+
+
 )
-def figWithNewDf(selected_year, selected_month):
+def figWithNewDf(selected_year, selected_month, selected_region):#, selected_price):
     print(selected_year)
     print(selected_month)
+    print(selected_region)
     print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
-    new_df_selected = preproc.group_by_year_month(df, int(selected_year), selected_month)
-    #if new_df_selected.empty:
-        #pymsgbox.alert('Pas d''événements pour la période choisie.', 'Avertissement')
-    return stackedBarChart.stackedBarChart(preproc.group_by_year_month(df, int(selected_year), selected_month))
-        
+    if selected_region == 'Toutes les régions':
+        return stackedBarChart.stackedBarChart(preproc.group_by_year_month(
+            df, int(selected_year), selected_month))
+    else:
+        return stackedBarChart.stackedBarChart(preproc.group_by_year_month_region(
+            df, int(selected_year), selected_month, selected_region))
+        #print('HERE!!!!!', selected_price)
+    # if new_df_selected.empty:
+        # pymsgbox.alert('Pas d''événements pour la période choisie.', 'Avertissement')
