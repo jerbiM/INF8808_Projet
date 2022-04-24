7a75f9786540159a18fb4046fc871ea54d95a371
Ghali Harti <hghali26@gmail.com>
2022-04-24 15:06:46 -0400

    started to add other radio buttons

--- code/src/heatmap.py
+++ code/src/heatmap.py
@@ -1,42 +1,79 @@
-from plotly_calplot import calplot
+# from plotly_calplot import calplot
+from calplot import calplot
 from datetime import datetime
 import numpy as np
 from pandas import read_excel, read_csv
 import pandas as pd
 from collections import Counter
+import matplotlib.pyplot as plt
 
+##################### Filter function #############################
+def filter_years(df, years: list):
+  if years == [0]:
+    return df
+  else: 
+    years_to_keep = set(years)
+    return df[df.year.isin(years_to_keep)]
+  
+def filter_category(df, categories: list):
+  if categories == ["all"]:
+    return df
+  else: 
+    categories_to_keep = set(categories)
+    return df[df.categorie.isin(categories_to_keep)]
 
+def filter_price(df, pricing: str): 
+  if pricing == "all": 
+    return df
+  else:
+    if pricing == "free":
+      return df[df.est_gratuit]
+    elif pricing == "pricy":
+      return df[df.est_gratuit == False]
+
+def filter_modality(df, modality: str):
+  if modality == "all": 
+    return df
+  else:
+    if modality == "online":
+      return df[df.est_enLigne]
+    elif modality == "inperson":
+      return df[df.est_enLigne == False]
+
+def filter_df(df, years = [0], categories = ["all"], pricing = "all", modality = "all"):
+  df1 = filter_modality(df, modality)
+  df2 = filter_price(df1, pricing)
+  df3 = filter_category(df2, categories)
+  return filter_years(df3, years)  
+
+##################### Potting function #############################
 def get_dates_count(dates:list):
   count = Counter(dates)
   dates = list(count.keys())
   counts = list(count.values())
   return pd.DataFrame(list(zip(dates, counts)),
                columns =['date', 'count']).sort_values(by='date',ignore_index=True)
+  
+  
+def plot_heatmap(df, title: str):
+  dates = pd.to_datetime(df.date_debut).to_list()
+  dates_count = get_dates_count(dates)
+  dates_count.set_index("date", inplace=True)
+  count_data = pd.Series(dates_count["count"])
+  n_fig = len(df.year.unique())
+  calplot(data=count_data, cmap='Reds', figsize=(18, n_fig*3))
+  plt.suptitle(title, y=1.10, fontsize=20)
+  
+def filter_plot_heatmap(df, years: list, categories: list, pricing: str, modality: str):
+  prefiltred_df = filter_df(df, years=years, categories=categories)
+  
+  if(pricing != "compare"): 
+    df_filtred = filter_df(prefiltred_df, pricing=pricing, modality=modality)
+    plot_heatmap(df_filtred, title="Cartes de chaleur selon les filtres")
 
-
-def filter_years(df, years:list):
-  data_years = df[df.year.isin(years)].date_debut.to_list()
-  return [pd.to_datetime(d) for d in data_years]
-
-
-def make_heatmap(df, years):
-    data_years = filter_years(df, years)
-    df_day_count = get_dates_count(data_years)
-    fig = calplot(
-        df_day_count,
-        x="date",
-        y="count",
-        name="Number of events",
-        years_title=True,
-        space_between_plots = 0.18,
-    )
-    fig.update_traces(colorbar=dict(
-    title="Nombre d'évènements",
-    thicknessmode="pixels", thickness=10,
-    lenmode="pixels", len=300,
-    yanchor="middle",
-    ticks="outside", 
-    ticksuffix="",
-    dtick=2
-    ), showscale = True, selector=dict(type='heatmap'))
-    return fig
+  elif(pricing == "compare"): 
+    df_filtred2 = filter_df(prefiltred_df, modality=modality)
+    df_free = filter_df(df_filtred2, pricing = "free")
+    df_pricy = filter_df(df_filtred2, pricing = "pricy")
+    plot_heatmap(df_free, title="Carte(s) de chaleur des évènements gratuits")
+    plot_heatmap(df_pricy, title="Carte(s) de chaleur des évènements payants")
--- code/src/test_radio.py
+++ code/src/test_radio.py
@@ -1,31 +1,148 @@
-from dash import Dash, dcc
-from dash.dependencies import Input, Output
+from dash import Dash, dcc,html
+import dash_bootstrap_components as dbc
 
+from dash.dependencies import Input, Output
+import heatmap
+import preprocess as preproc
+import os
+
+
+# print("Current path : " + os.getcwd())
+# print("Folders in path : ", os.listdir('.'))
+# os.chdir("code/src/assets")
+# print("Current path : " + os.getcwd())
+# print("Folders in path : ", os.listdir('.'))
+df_file_preprocessed = "code/src/assets/df_ghali_v3.csv"
+df_preprocessed = preproc.to_df(df_file_preprocessed)
+
+fig4 = heatmap.make_heatmap(df_preprocessed, years=set([2021]))
 
 app = Dash(__name__)
 
-app.layout = dcc.RadioItems(
-   options={
-        'New York City': 'New York City',
-        'Montreal': 'Montreal',
-        'San Francisco': 'San Francisco'
-   },
-   value='Montreal',
-    id="testradio"
+app.layout = html.Div([
 
-   ,inline=True)
+#######################################################################
+# Radio buttons for year
+#######################################################################
+html.Div(
+    [
+        dbc.Label("Année : "),
+        dbc.Checklist(
+            options=[
+                {"label": "2016", "value": 2016},
+                {"label": "2017", "value": 2017},
+                {"label": "2018", "value": 2018},
+                {"label": "2019", "value": 2019},
+                {"label": "2020", "value": 2020},
+                {"label": "2021", "value": 2021},
+                {"label": "2022", "value": 2022},
+                {"label": "2023", "value": 2023},
+                {"label": "Toutes les années", "value": 0},
+            ],
+            value=[2021],
+            id="bullets_year",
+        ),
+    ]
+),
+#######################################################################
 
-viz_heatmap = 
+#######################################################################
+# Radio buttons for category
+#######################################################################
+html.Div(
+    [
+        dbc.Label("Catégorie : "),
+        dbc.Checklist(
+            options=[
+                {"label": "Musique", "value": "Musique"},
+                {"label": "Théatre", "value": "Théâtre"},
+                {"label": "Arts visuels", "value": "ArtsVisuels"},
+                {"label": "Humour", "value": "Humour"},
+                {"label": "Danse", "value": "Danse"},
+                {"label": "Cirque", "value": "Cirque"},
+                {"label": "Autre", "value": "ÉvénementielAutre"},
+                {"label": "Toute catégorie", "value": "all"},
+            ],
+            value="all",
+            id="bullets_category",
+        ),
+    ]
+),
+#######################################################################
+
+#######################################################################
+# Radio buttons for price
+#######################################################################
+html.Div(
+    [
+        dbc.Label("Prix : "),
+        dbc.Checklist(
+            options=[
+                {"label": "Gratuit", "value": "free"},
+                {"label": "Payant", "value": "pricy"},
+                {"label": "Voir tout", "value": "all"},
+                {"label": "Comparer gratuit et payant", "value": "compare"},
+            ],
+            value="all",
+            id="bullets_price",
+        ),
+    ]
+),
+#######################################################################
+
+#######################################################################
+# Radio buttons for Modality
+#######################################################################
+html.Div(
+    [
+        dbc.Label("Modalité : "),
+        dbc.Checklist(
+            options=[
+                {"label": "Virtuel", "value": "online"},
+                {"label": "Présentiel", "value": "inperson"},
+                {"label": "Voir tout", "value": "all"},
+                {"label": "Comparer virtuel et présentiel", "value": "compare"},
+            ],
+            value="all",
+            id="bullets_modality",
+        ),
+    ]
+),
+#######################################################################
+
+#######################################################################
+# Viz
+#######################################################################
+    html.Div(className='viz-container', children=[
+    dcc.Graph(
+        figure=fig4,
+        config=dict(
+            scrollZoom=False,
+            showTips=False,
+            showAxisDragHandles=False,
+            displayModeBar=False
+        ),
+        className='graph',
+        id='viz_4'
+    )
+])
+#######################################################################
+                 ])
+
+
 
 @app.callback(
 
-    Output("viz_heatmap", 'figure'),
-    Input('testradio', 'value')
+    Output("viz_4", 'figure'),
+    Input('bullets_year', 'value')
    # [Input(component_id='radio-selected-style', component_property='value')],
 )
 
-def showfig8(testradio):
-    print(testradio)
+def showfig4(testradio):
+    print("Year is ",testradio)
+    return heatmap.make_heatmap(df_preprocessed, years=set(testradio))
+
+
 
 
 if __name__ == "__main__":
